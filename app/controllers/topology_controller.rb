d388841d50528fc2f165687b1ae0aa5fd46bb91e
nemunemu <e155719@ie.u-ryukyu.ac.jp>
2017-11-22 15:49:39 +0900

    fix edit

--- app/controllers/topology_controller.rb
+++ app/controllers/topology_controller.rb
@@ -1,6 +1,5 @@
 class TopologyController < ApplicationController
   def show
-    # @topologies = Topology.select("id")
     @topologies = Topology.all
     gon.topologies = @topologies
   end
@@ -21,9 +20,9 @@ class TopologyController < ApplicationController
 
   def update
     if Vlan.find_by(start: params[:start])
-      Vlan.find_by(start: params[:start]).update_attribute(:path, params[:path])
+      Vlan.find_by(start: params[:start]).update_attributes(start: params[:start], end: params[:end], path: params[:path])
     elsif Vlan.find_by(end: params[:start])
-      Vlan.find_by(end: params[:start]).update_attribute(:path, params[:path])
+      Vlan.find_by(end: params[:start]).update_attributes(start: params[:start], end: params[:end], path: params[:path])
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  # get 'topology/show'
-  # get 'topology/edit'
   get 'topology/update'
   get 'topology/addvlan'
 
   root to: 'topology#show'
   get 'edit', to: 'topology#edit'
-  get 'modify', to: 'topology#modify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
